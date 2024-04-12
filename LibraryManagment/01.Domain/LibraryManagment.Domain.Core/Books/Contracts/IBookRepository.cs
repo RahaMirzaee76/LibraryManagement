1a618426b3a2a8c7f81e4f2051d518a2c9fbf8f9
S.Mahdi Hoseini <mmhf6380@gmail.com>
2024-04-12 13:00:20 +0330

    Add Book Module Entites And Services

---
+++ LibraryManagment/01.Domain/LibraryManagment.Domain.Core/Books/Contracts/IBookRepository.cs
@@ -0,0 +1,13 @@
+﻿using LibraryManagment.Domain.Core.Books.Entities;
+
+namespace LibraryManagment.Domain.Core.Books.Contracts
+{
+    public interface IBookRepository
+    {
+        List<Book> GetAll();
+        Book GetById(int id);
+        Book Add(Book book);
+        Book Update(Book book); 
+        Book Delete(int id);    
+    }
+}
---
+++ LibraryManagment/01.Domain/LibraryManagment.Domain.Core/Books/Entities/Book.cs
@@ -0,0 +1,19 @@
+﻿using LibraryManagment.Domain.Core.Common.Entities;
+
+namespace LibraryManagment.Domain.Core.Books.Entities
+{
+    public class Book : BaseEntity
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Publisher { get; set; }
+        public string Translator { get; set; }
+        public string Shabak { get; set; }
+        public DateTime PublishDate { get; set; }
+        public int PageCount { get; set; }
+        public DateTime RegisterDate { get; set; }
+        public DateTime UpdateDate { get; set; }
+        public bool IsActive { get; set; }
+        public bool IsReserved { get; set; }
+    }
+}
